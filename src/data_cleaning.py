31697b71a1e5aacc57e93e540601acc42b5c8851
jaskirat0810 <jaskirat0810@gmail.com>
2023-12-02 16:02:44 -0500

    Loaded Yaml file and testing basic functionality

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,5 +1,26 @@
 import pandas as pd
+from typing import Optional, List
+
+from logger import get_logger
 
 
-def get_cleaned_data() -> pd.DataFrame:
-    pass
+pd.set_option("display.max_colwidth", 100)
+logger = get_logger(module_name="data-cleaning")
+
+
+def load_csv_data(input_path: str, columns: Optional[List[str]] = None) -> pd.DataFrame:
+    if columns is not None:
+        return pd.read_csv(
+            filepath_or_buffer=input_path,
+            header=0,
+            usecols=columns
+        )
+    else:
+        return pd.read_csv(input_path)
+
+
+def get_cleaned_data(data_path: str) -> pd.DataFrame:
+    used_cars: pd.DataFrame = load_csv_data(input_path=data_path)
+    logger.info(used_cars.info())
+
+    return used_cars
--- src/runner.py
+++ src/runner.py
@@ -1,5 +1,18 @@
 import yaml
+from yaml.loader import SafeLoader
+
+from logger import get_logger
 
 
-def run_app(config_file: str):
-    pass
+# pd.set_option("display.max_colwidth", 100)
+logger = get_logger(module_name="data-cleaning")
+
+
+def parse_config_file(file_path: str):
+    with open(file_path, "r") as f:
+        config_data = list(yaml.load_all(f, Loader=SafeLoader))
+        logger.info(config_data)
+
+
+def run_app(config_file_path: str):
+    parse_config_file(file_path=config_file_path)
